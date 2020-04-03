8ece691b7fd553b3f17e31995410e688836dfa29
s445348 <adam.winogron@ocsbstudent.ca>
2020-04-03 16:32:24 -0400

    worked on it

--- MathFunAdam/main.lua
+++ MathFunAdam/main.lua
@@ -29,6 +29,17 @@ local correctSound = audio.loadSound("Sounds/correctSound.mp3")
 local correctSoundChannel
 local incorrectSound = audio.loadSound("Sounds/incorrectSound.mp3")
 local incorrectSoundChannel
+
+local totalSeconds = 10
+local secondsLeft = 10
+local clockText
+local countDownTimer
+
+local lives = 5
+local heart1
+local heart2
+local heart3
+local heart4
 ----------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 ----------------------------------------------------------------------------------------
@@ -111,13 +122,59 @@ local function NumericFieldListener( event )
 			correctObject.isVisible = true
 			timer.performWithDelay(1900, HideCorrect)
 			correctSoundChannel = audio.play(correctSound)
+			secondsLeft = totalSeconds
 		else
 			-- if the users answer and the correct answer are different
 			incorrectObject.isVisible = true
 			timer.performWithDelay(1900, HideIncorrect)
 			incorrectSoundChannel = audio.play(incorrectSound)
+			lives = lives - 1
+			secondsLeft = totalSeconds
+
 		end
 	end
+end
+
+
+local function UpdateTime()
+
+	--decrement the number of seconds
+	secondsLeft = secondsLeft - 1
+
+	-- display the number of seconds left in the clock object
+	clockText.text = "Time Left: " .. secondsLeft
+
+	if (secondsLeft == 0 ) then
+		-- reset the number of seconds left 
+		secondsLeft = totalSeconds
+		lives = lives - 1
+	end
+end
+
+local function LoseLives()
+	if (lives == 4) then
+		heart4.isVisible = false
+	elseif (lives == 3) then
+		heart3.isVisible = false
+	elseif (lives == 2) then
+		heart2.isVisible = false
+	elseif (lives == 1) then
+		heart1.isVisible = false
+	end
+end
+
+local function Lose()
+	if (lives == 0) then
+		loseText.isVisible = true
+		loseImage,isVisible = true
+		timer.cancel(countDownTimer)
+	end
+end
+
+
+-- function that calls the timer
+local function StartTimer()
+	countDownTimer = timer.performWithDelay( 1000, UpdateTime, 0 )
 end
 
 -----------------------------------------------------------------------------------------
@@ -143,9 +200,42 @@ incorrectObject = display.newText( "Incorrect!", display.contentWidth/2, display
 incorrectObject:setTextColor( 50/255, 0/255, 255/255 )
 incorrectObject.isVisible = false
 
+heart1 = display.newImageRect("Images/life.png", 100, 100)
+heart1.x = display.contentWidth * 7 / 8
+heart1.y = display.contentHeight * 1 / 7
+
+heart2 = display.newImageRect("Images/life.png", 100, 100)
+heart2.x = display.contentWidth * 6 / 8
+heart2.y = display.contentHeight * 1 / 7
+
+heart3 = display.newImageRect("Images/life.png", 100, 100)
+heart3.x = display.contentWidth * 5 / 8
+heart3.y = display.contentHeight * 1 / 7
+
+heart4 = display.newImageRect("Images/life.png", 100, 100)
+heart4.x = display.contentWidth * 4 / 8
+heart4.y = display.contentHeight * 1 / 7
+
+clockText = display.newText("Time Left: ", 200, 100, nil, 50)
+clockText:setTextColor(0,1,1)
+
+loseText = display.newText("Game Over!", display.contentWidth/2, display.contentHeight/1.7, nil, 100)
+loseText.isVisible = false
+
+loseImage = display.newImageRect("Images/lose.gif", 458, 238)
+loseImage.x = display.contentWidth/2
+loseImage.y = display.contentHeight/1.22
+loseImage.isVisible = false
+
 -----------------------------------------------------------------------------------------
 -- FUNCTION CALLS
 -----------------------------------------------------------------------------------------
 
 -- call the function to ask the question
 AskQuestion()
+
+StartTimer()
+
+LoseLives()
+
+Lose()
