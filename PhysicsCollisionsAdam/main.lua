ddf785c2298fde59976cc2029be1c08c15a3c162
s445348 <adam.winogron@ocsbstudent.ca>
2020-04-06 17:17:30 -0400

    k

--- PhysicsCollisionsAdam/main.lua
+++ PhysicsCollisionsAdam/main.lua
@@ -18,26 +18,26 @@ physics.start()
 -----------------------------------------------------------------------------------------
 
 --Ground
-local ground = display.newImage("Physics/ground.png", 0, 0)
+local ground = display.newImage("Images/ground.png", 0, 0)
 
 	-- change the width to be the same as the screen
 	ground.width = display.contentWidth
 
 	-- set the x and y pos
 	ground.x = display.contentWidth/2
-	ground.y = display.contnetHeight/10
+	ground.y = display.contentHeight* 96/100
 
 	-- add to physics
 	physics.addBody(ground, "static", {friction = 0.5, bounce = 0.3})
 
-local beam = display.newImage("Physics/ground.png", 0, 0)
+local beam = display.newImage("Images/ground.png", 0, 0)
 
 	-- set the x and y pos
 	beam.x = display.contentCenterX/5
 	beam.y = display.contentCenterY*1.65
 
 	beam.width = display.contentWidth/2
-	beam.height = display.contnetHeight* 1/10
+	beam.height = display.contentHeight* 1/10
 
 	-- rotate the beam -60 degrees so it's on an angle
 	beam:rotate(45)
@@ -49,7 +49,7 @@ local beam = display.newImage("Physics/ground.png", 0, 0)
 	physics.addBody(beam, "static", {friction = 0.5, bounce = 0.3})
 
 -- create bgk
-local bkg = display.newImage("Physics/bkg.png", 0, 0)
+local bkg = display.newImage("Images/bkg.png", 0, 0)
 	
 	--set the x and y pos
 	bkg.x = display.contentCenterX
@@ -68,7 +68,7 @@ local bkg = display.newImage("Physics/bkg.png", 0, 0)
 -- create the firat ball
 local function FirstBall()
 	-- creating first ball
-	local ball1 = display.newImage("Physics/super_ball.png", 0, 0)
+	local ball1 = display.newImage("Images/super_ball.png", 0, 0)
 
 	-- add to physics
 	physics.addBody(ball1, {density = 1.0, friction = 0.5, bounce = 0.3, radius = 25})
@@ -76,10 +76,10 @@ end
 
 local function SecondBall()
 	--create the second ball
-	local ball2 = display.newImage("Physics/super_ball.png", 0, 0)
+	local ball2 = display.newImage("Images/super_ball.png", 0, 0)
 
 	-- add to physics
-	physics.addBody(ball2, {density = 1.0, density = 1.0, friction = 0.5, bounce = 0.3, radius = 12.5})
+	physics.addBody(ball2, {density = 1.0, friction = 0.5, bounce = 0.3, radius = 12.5})
 end
 
 -----------------------------------------------------------------------------------------
