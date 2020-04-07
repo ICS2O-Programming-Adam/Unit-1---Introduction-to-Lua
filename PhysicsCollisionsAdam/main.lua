1e0873465deef434ce9fcaa8dd962b5c28b9e91a
s445348 <adam.winogron@ocsbstudent.ca>
2020-04-07 16:38:07 -0400

    w

--- PhysicsCollisionsAdam/main.lua
+++ PhysicsCollisionsAdam/main.lua
@@ -13,6 +13,10 @@ local physics = require("physics")
 -- start physics
 physics.start()
 
+-- play the background music 
+local soundChannel = audio.loadStream("Sounds/Music.mp3")
+audio.play(soundChannel, {loops = -1})
+
 -----------------------------------------------------------------------------------------
 -- Objects
 -----------------------------------------------------------------------------------------
@@ -30,7 +34,7 @@ local ground = display.newImage("Images/ground.png", 0, 0)
 	-- add to physics
 	physics.addBody(ground, "static", {friction = 0.5, bounce = 0.3})
 
-local beam = display.newImage("Images/ground.png", 0, 0)
+local beam = display.newImage("Images/beam.png", 0, 0)
 
 	-- set the x and y pos
 	beam.x = display.contentCenterX/5
@@ -46,7 +50,26 @@ local beam = display.newImage("Images/ground.png", 0, 0)
 	beam:toBack()
 
 	-- add to physics
-	physics.addBody(beam, "static", {friction = 0.5, bounce = 0.3})
+	physics.addBody(beam, "static", {friction = 0.5,})
+
+local wall = display.newImage("Images/beam_long.png", 0, 0)
+	
+	-- set the x and y pos
+	wall.x = display.contentWidth/1.05
+	wall.y = display.contentHeight/2
+
+	-- set the dimensions of the wall
+	wall.width = display.contentWidth/10
+	wall.height = display.contentHeight
+
+	-- rotate the wall so it's vertical
+	--wall:rotate(90)
+
+	-- send it to the back layer
+	wall:toBack()
+
+	-- add to physics
+	physics.addBody(wall, "static", {friction = 0.5, bounce = 0.3})
 
 -- create bgk
 local bkg = display.newImage("Images/bkg.png", 0, 0)
@@ -61,6 +84,8 @@ local bkg = display.newImage("Images/bkg.png", 0, 0)
 	-- send to back
 	bkg:toBack()
 
+
+
 -----------------------------------------------------------------------------------------
 -- LOCAL FUNCTIONS
 -----------------------------------------------------------------------------------------
@@ -71,7 +96,7 @@ local function FirstBall()
 	local ball1 = display.newImage("Images/super_ball.png", 0, 0)
 
 	-- add to physics
-	physics.addBody(ball1, {density = 1.0, friction = 0.5, bounce = 0.3, radius = 25})
+	physics.addBody(ball1, {density = 5.0, friction = 0.1, bounce = 1, radius = 25})
 end
 
 local function SecondBall()
@@ -82,8 +107,25 @@ local function SecondBall()
 	physics.addBody(ball2, {density = 1.0, friction = 0.5, bounce = 0.3, radius = 12.5})
 end
 
+local function ThirdBall()
+	--create the third ball
+	local ball3 = display.newImage("Images/super_ball.png", 0, 0)
+
+	-- add to physics
+	physics.addBody(ball3, {density = 1.0, friction = 0.5, bounce = 0.8, radius = 4})
+end
+
+local function FourthBall()
+	--create the fourth ball
+	local ball4 = display.newImage("Images/super_ball.png", 0, 0)
+
+	-- add to physics
+	physics.addBody(ball4, {density = 1.0, friction = 0.5, bounce = 0.8, radius = 75})
+end
 -----------------------------------------------------------------------------------------
 -- TIMER DELAYS - call each function after the given millisecond
 -----------------------------------------------------------------------------------------
-timer.performWithDelay( 0, firstBall )
-timer.performWithDelay( 500, SecondBall)
+timer.performWithDelay( 0, FirstBall )
+timer.performWithDelay( 500, SecondBall )
+timer.performWithDelay( 1000, ThirdBall )
+timer.performWithDelay( 1500, FourthBall )
